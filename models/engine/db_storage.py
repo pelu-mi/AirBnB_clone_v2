4156f17b94a41c476715ea920d614dbc196572bd
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-01-06 18:52:25 +0100

    Testing task 6

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -84,6 +84,7 @@ class DBStorage:
         Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(Session)()
 
-    def close():
-        """ Close the session"""
+    """def close():
+        "" Close the session""
         self.__session.close()
+        """
