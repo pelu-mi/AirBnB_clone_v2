87b7a471d53b9743e5e49564abdf9c745bcdacba
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-01-06 18:41:01 +0100

    Fix 2 for task 6;

--- models/city.py
+++ models/city.py
@@ -11,7 +11,7 @@ class City(BaseModel, Base):
     if storage_env == 'db':
         # For DBStorage
         name = Column(String(128), nullable=False)
-        state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     else:
         name = ''
         state_id = ''
