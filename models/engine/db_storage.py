dc42a9e080f18d3617b93931a86af8b622304432
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-01-06 20:10:30 +0100

    Test 6 fix 2 for task 6

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -12,8 +12,6 @@ from models.state import State
 from models.user import User
 from os import getenv
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    from models.place import place_amenity
 
 classes = {"User": User, "State": State, "City": City,
            "Amenity": Amenity, "Place": Place, "Review": Review}
