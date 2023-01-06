994278a92c682a0444e22a76df842197244f8146
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-01-06 18:37:20 +0100

    Fix 1 for task 6

--- models/city.py
+++ models/city.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models import storage_env
 from sqlalchemy import Column, String, ForeignKey
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
+    if storage_env == 'db':
+        # For DBStorage
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
+    else:
+        name = ''
+        state_id = ''
--- models/state.py
+++ models/state.py
@@ -1,13 +1,34 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models import storage_env
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state',
+    __tablename__ = "states"
+
+    if storage_env == 'db':
+        # For DBStorage
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
                           cascade='all, delete, delete-orphan')
+    else:
+        # For FileStorage
+        name = ''
+
+        @property
+        def cities(self):
+            """ Return list of City instances where
+                state_id = current State.id
+            """
+            from models import storage
+            related_cities = []
+            all_cities = storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    related_cities.append(city)
+            return related_cities
