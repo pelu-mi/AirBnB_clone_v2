52e9bc209cc8c42dee948c83813e43e5a42f16c5
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-01-06 18:15:17 +0100

    Added files and updates for task 6

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+"""This module instantiates the storage based on the environment variable """
+from os import getenv
 
 
-storage = FileStorage()
+storage_env = getenv('HBNB_TYPE_STORAGE')
+if storage_env == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+# Reload the storage
 storage.reload()
