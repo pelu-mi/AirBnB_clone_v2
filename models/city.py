619b0eeec827ba2d10bbee62062dcc3979fb6af4
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-01-07 00:54:40 +0100

    Added changes for task 8

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from models import storage_type
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -12,6 +13,8 @@ class City(BaseModel, Base):
         # For DBStorage
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', backref='cities',
+                              cascade='all delete delete-orphan')
     else:
         name = ''
         state_id = ''
